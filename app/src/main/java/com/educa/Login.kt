87010438232be5bf939123e973d66fcf732b79eb
julianaesteves <juliana.trindade@bandtec.com.br>
2023-04-16 00:06:16 -0300

    feat: dependência retrofit e gson, autenticação e deletando itens

---
+++ app/src/main/java/com/educa/Login.kt
@@ -0,0 +1,66 @@
+package com.educa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.educa.api.service.SessionManager
+import com.educa.api.model.LoginRequest
+import com.educa.api.model.LoginResponse
+import com.educa.api.service.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class Login : AppCompatActivity() {
+    private lateinit var sessionManager: SessionManager
+    private lateinit var apiClient: ApiClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        apiClient = ApiClient()
+        sessionManager = SessionManager(this)
+
+        val emailField = findViewById<EditText>(R.id.ipt_email)
+        val email = emailField.text.toString()
+
+        val passwordField = findViewById<EditText>(R.id.ipt_password)
+        val password = passwordField.text.toString()
+
+        val loginButton = findViewById<Button>(R.id.btn_cadastro)
+
+        /*
+        loginButton.setOnClickListener {
+            apiClient.getAuthApiService().login(
+                LoginRequest(email, password)
+                    .enqueue(object : Callback<LoginResponse> {
+
+                        override fun onResponse(
+                            call: Call<LoginResponse>,
+                            response: Response<LoginResponse>
+                        ) {
+                            val loginResponse = response.body()
+
+                            if (loginResponse?.statusCode == 200) {
+                                sessionManager.saveAuthToken(loginResponse.token)
+                            } else {
+                                // Error logging in
+                            }
+                        }
+
+                        override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                            Toast.makeText(
+                                baseContext, "Erro na API: ${t.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            t.printStackTrace()
+                        }
+                    })
+            )
+        }
+         */
+    }
+}
--- app/src/main/java/com/educa/SignUp.kt
+++ app/src/main/java/com/educa/SignUp.kt
@@ -3,18 +3,51 @@ package com.educa
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.educa.api.model.Student
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SignUp : AppCompatActivity() {
     private lateinit var tvDatePicker: com.google.android.material.textfield.TextInputEditText
-    private lateinit var clickCalndar: com.google.android.material.textfield.TextInputEditText
+    private lateinit var clickCalendar: com.google.android.material.textfield.TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        tvDatePicker = findViewById(R.id.editDate)
-        clickCalndar = findViewById(R.id.editDate)
+
+        val signUpButton = findViewById<Button>(R.id.btn_cadastro)
+        signUpButton.setOnClickListener {
+            val nameField = findViewById<EditText>(R.id.ipt_name)
+            val name = nameField.text.toString()
+
+            val lastNameField = findViewById<EditText>(R.id.ipt_lastName)
+            val lastName = lastNameField.text.toString()
+
+            val birthdateField = findViewById<EditText>(R.id.ipt_birthdate)
+            val birthdate = birthdateField.text.toString()
+
+            val emailField = findViewById<EditText>(R.id.ipt_email)
+            val email = emailField.text.toString()
+
+            val passwordField = findViewById<EditText>(R.id.ipt_password)
+            val password = passwordField.text.toString()
+
+            val confirmPasswordField = findViewById<EditText>(R.id.ipt_confirmPassword)
+            val confirmPassword = confirmPasswordField.text.toString()
+
+            val newStudent = Student(
+                nome = name,
+                sobrenome = lastName,
+                email =  email,
+                dataNasc = birthdate,
+                senha = password
+            )
+        }
+
+        tvDatePicker = findViewById(R.id.ipt_birthdate)
+        clickCalendar = findViewById(R.id.ipt_birthdate)
 
         val myCalendar = Calendar.getInstance()
 
@@ -26,7 +59,7 @@ class SignUp : AppCompatActivity() {
 
         }
 
-        clickCalndar.setOnClickListener {
+        clickCalendar.setOnClickListener {
             DatePickerDialog(
                 this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)
