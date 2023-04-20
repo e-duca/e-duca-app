565ff31b08a6ee0878c9fd9ca0178272d3967f7f
Andre Sena <89034251+gitsena@users.noreply.github.com>
2023-04-20 19:58:31 -0300

    fix: correcao nomenclatura, traducao para o ingles (#13)
    * fix: correcao nomenclatura, traducao para o ingles
    
    * fix: add network security no android manifest
    
    ---------
    
    Co-authored-by: julianaesteves <juliana.trindade@bandtec.com.br>

--- app/src/main/java/com/educa/FragmentNavBar.kt
+++ app/src/main/java/com/educa/FragmentNavBar.kt
@@ -5,18 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CardContent.newInstance] factory method to
+ * Use the [FragmentNavBar.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CardContent : Fragment() {
-    // TODO: Rename and change types of parameters
+class FragmentNavBar : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
@@ -28,10 +27,12 @@ class CardContent : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_content, container, false)
+        return inflater.inflate(R.layout.fragment_navbar, container, false)
     }
 
     companion object {
@@ -41,15 +42,15 @@ class CardContent : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CardContent.
+         * @return A new instance of fragment NavBar.
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                CardContent().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FragmentNavBar().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
+            }
     }
 }
