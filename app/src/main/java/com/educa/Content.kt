819d224b4d957e38a5983ff8b46083e7c41f6fdf
julianaesteves <juliana.trindade@bandtec.com.br>
2023-04-25 19:42:33 -0300

    fix: fragment onClickListener

--- app/src/main/java/com/educa/Content.kt
+++ app/src/main/java/com/educa/Content.kt
@@ -10,16 +10,16 @@ class Content : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
 
-        val card01 = findViewById<TextView>(R.id.fg_card01)
+        val card01 = findViewById<androidx.fragment.app.FragmentContainerView>(R.id.fg_card01)
         card01.setOnClickListener {
             val readingPage = Intent(applicationContext, Reading::class.java)
             startActivity(readingPage)
         }
 
-        val card02 = findViewById<TextView>(R.id.fg_card02)
+        val card02 = findViewById<androidx.fragment.app.FragmentContainerView>(R.id.fg_card02)
         card02.setOnClickListener {
-            // val videoPage = Intent(applicationContext, Video::class.java)
-            // startActivity(videoPage)
+            val videoPage = Intent(applicationContext, Video::class.java)
+            startActivity(videoPage)
         }
     }
 }
