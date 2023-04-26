fd5761f1aff95d9d915a3a160cb473adfc79b646
gitsena <andre.sena@bandtec.com.br>
2023-04-26 04:21:14 -0300

    fix: input data e calendario funcional

--- app/src/main/java/com/educa/SignUp.kt
+++ app/src/main/java/com/educa/SignUp.kt
@@ -1,14 +1,17 @@
 package com.educa
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.educa.api.model.Student
 import com.educa.api.service.ApiClient
 import com.educa.api.service.SessionManager
@@ -16,6 +19,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.*
 
 class SignUp : AppCompatActivity() {
@@ -53,7 +57,7 @@ class SignUp : AppCompatActivity() {
             val lastName = lastNameField.text.toString()
 
             val birthdateField = findViewById<EditText>(R.id.ipt_birthdate)
-            val birthdate = birthdateField.text.toString()
+            val birthdate = updateLableBack(birthdateField.text.toString())
 
             val emailField = findViewById<EditText>(R.id.ipt_email)
             val email = emailField.text.toString()
@@ -96,6 +100,33 @@ class SignUp : AppCompatActivity() {
             }
         }
 
+        val ipt_birthdate = findViewById<EditText>(R.id.ipt_birthdate)
+
+        ipt_birthdate.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val digit = ipt_birthdate.text.toString()
+
+                if (digit.length < 10) {
+                    if (count == 1 && digit.length == 2 || digit.length == 5) {
+                        ipt_birthdate.setText(
+                            StringBuilder(digit).insert(
+                                digit.length,
+                                "-"
+                            ).toString()
+                        )
+
+                        ipt_birthdate.setSelection(ipt_birthdate.text.length)
+                    }
+                }
+            }
+        })
+
         tvDatePicker = findViewById(R.id.ipt_birthdate)
         clickCalendar = findViewById(R.id.ipt_birthdate)
 
@@ -108,7 +139,7 @@ class SignUp : AppCompatActivity() {
             updateLable(myCalendar)
         }
 
-        clickCalendar.setOnClickListener {
+        tvDatePicker.setOnClickListener {
             DatePickerDialog(
                 this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)
@@ -120,6 +151,16 @@ class SignUp : AppCompatActivity() {
         val myFormat = "dd-MM-yyyy"
         val sdf = SimpleDateFormat(myFormat, Locale.UK)
         tvDatePicker.setText(sdf.format(myCalendar.time))
+    }
+
+    @SuppressLint("NewApi")
+    private fun updateLableBack(date: String): String {
+        val formatInput = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        val formatOutput = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+        val dataFormatter = formatOutput.format(formatInput.parse(date))
+
+        return LocalDate.parse(dataFormatter).toString()
     }
 
     fun signUp(newStudent: Student) {
@@ -157,7 +198,8 @@ class SignUp : AppCompatActivity() {
 
                 override fun onFailure(call: Call<Student>, t: Throwable) {
                     Toast.makeText(
-                        baseContext, "Erro no servidor! Por favor, tente novamente mais tarde. ERRO: ${t.message}",
+                        baseContext,
+                        "Erro no servidor! Por favor, tente novamente mais tarde. ERRO: ${t.message}",
                         Toast.LENGTH_SHORT
                     ).show()
                     t.printStackTrace()
