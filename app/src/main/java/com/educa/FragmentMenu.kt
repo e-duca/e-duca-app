ccf8307b90802ce0bd0facbeebe54a8388d823f5
Andre Sena <89034251+gitsena@users.noreply.github.com>
2023-04-29 19:50:04 -0300

    feat: menu funcional, correcoes no input data (#20)

--- app/src/main/java/com/educa/FragmentMenu.kt
+++ app/src/main/java/com/educa/FragmentMenu.kt
@@ -1,29 +1,47 @@
 package com.educa
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentMenu.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentMenu : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    private lateinit var btnOpenDrawer: ImageButton
+    private lateinit var btnCloseDrawer: ImageButton
+    private lateinit var drawerLayout: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    }
+
+    @Nullable
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnOpenDrawer = view.findViewById(R.id.btnOpenDrawer)
+        btnCloseDrawer = view.findViewById(R.id.btnCloseDrawer)
+        drawerLayout = view.findViewById(R.id.drawerLayout)
+
+        btnOpenDrawer.setOnClickListener {
+            if (drawerLayout.visibility == View.VISIBLE) {
+                drawerLayout.visibility = View.GONE
+            } else {
+                drawerLayout.visibility = View.VISIBLE
+            }
+        }
+
+        btnCloseDrawer.setOnClickListener {
+            if (drawerLayout.visibility == View.VISIBLE) {
+                drawerLayout.visibility = View.GONE
+            } else {
+                drawerLayout.visibility = View.VISIBLE
+            }
         }
     }
 
@@ -33,24 +51,8 @@ class FragmentMenu : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu, container, false)
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DrawerMenu.
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentMenu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
