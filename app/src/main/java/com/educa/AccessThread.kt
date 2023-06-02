c58f7323af8d027c03d1695a6bd1146bbecb5daf
andrediasdesena <andre.sena@bandtec.com.br>
2023-06-02 12:27:44 -0300

    fix: button, menu funcional Acess thread

--- app/src/main/java/com/educa/AccessThread.kt
+++ app/src/main/java/com/educa/AccessThread.kt
@@ -1,11 +1,20 @@
 package com.educa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AccessThread : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_access_thread)
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            val myQuestions = Intent(applicationContext, MyQuestions::class.java)
+            startActivity(myQuestions)
+        }
     }
 }
