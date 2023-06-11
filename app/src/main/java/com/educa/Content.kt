680a282519093ba1ac54e0009c7b6672f96e7e1a
andrediasdesena <andre.sena@bandtec.com.br>
2023-06-11 15:27:15 -0300

    feat: navBar funcional, loader content

--- app/src/main/java/com/educa/Content.kt
+++ app/src/main/java/com/educa/Content.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +17,7 @@ import com.educa.api.service.ApiClient
 import com.educa.api.service.SessionManager
 import com.educa.ui.adapters.ContentListAdapter
 import com.educa.ui.recyclerview.RecyclerViewInterface
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,7 +28,12 @@ class Content : AppCompatActivity(), RecyclerViewInterface {
     private lateinit var sessionManager: SessionManager
     lateinit var contentAdapter: ContentListAdapter
     lateinit var contentList: MutableList<ContentResponse>
-
+    private lateinit var searchView: SearchView
+    private lateinit var filteredContentList: MutableList<ContentResponse>
+    private lateinit var noContentText: RelativeLayout
+    private lateinit var loadingView: View
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private var loadingJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,67 +42,103 @@ class Content : AppCompatActivity(), RecyclerViewInterface {
         apiClient = ApiClient()
         sessionManager = SessionManager(this)
         contents = findViewById<RecyclerView>(R.id.rv_cards)!!
+        noContentText = findViewById(R.id.noContentText)
+        loadingView = findViewById(R.id.loadingView)
 
         loadContentList()
         Log.i(
             "CONTEÚDO: CHAMOU A FUNÇÃO",
             "loadContentList"
         )
+
+        searchView = findViewById(R.id.searchView)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                val normalizedQuery = normalizedText(newText)
+                performSearch(normalizedQuery)
+                return true
+            }
+        })
     }
 
+
     fun loadContentList() {
-        contentList = mutableListOf()
-        contentAdapter = ContentListAdapter(this, contentList, this)
+        loadingView.visibility = View.VISIBLE
 
-        val layoutManager = LinearLayoutManager(this)
+        loadingJob = coroutineScope.launch {
+            delay(1000)
 
-        contents.layoutManager = layoutManager
+            contentList = mutableListOf()
+            filteredContentList = mutableListOf() // Inicialize a lista aqui
 
-        contents.adapter = contentAdapter
+            contentAdapter = ContentListAdapter(this@Content, filteredContentList, this@Content)
 
-        Log.e(
-            "CONTEÚDO: ENTROU NA FUNÇÃO LoadContentList e puxou o token",
-            "FETCHED TOKEN: ${sessionManager.fetchAuthToken()}"
-        )
+            val layoutManager = LinearLayoutManager(this@Content)
 
-        apiClient.getMainApiService(this)
-            .getAllContent()
-            .enqueue(object : Callback<ContentResponseArray> {
-                @SuppressLint("NotifyDataSetChanged")
-                override fun onResponse(
-                    call: Call<ContentResponseArray>,
-                    response: Response<ContentResponseArray>
-                ) {
-                    if (response.isSuccessful) {
-                        response.body()?.content?.let { contentList.addAll(it) }
-                        contentAdapter.notifyDataSetChanged()
+            contents.layoutManager = layoutManager
 
-                        Log.i(
-                            "CONTEÚDO: ENTROU NO ISSUCCESSFUL",
-                            "Call: ${call} Response: ${response.body()}"
-                        )
+            contents.adapter = contentAdapter
 
-                    } else {
-                        Log.i(
-                            "CONTEÚDO: ENTROU NO IF DO ISSUCCESSFUL MAS CAIU NO ELSE",
-                            "Call: ${call}, Response: ${response.code()} ${response.body()})"
-                        )
+            Log.e(
+                "CONTEÚDO: ENTROU NA FUNÇÃO LoadContentList e puxou o token",
+                "FETCHED TOKEN: ${sessionManager.fetchAuthToken()}"
+            )
+
+            apiClient.getMainApiService(this@Content)
+                .getAllContent()
+                .enqueue(object : Callback<ContentResponseArray> {
+                    @SuppressLint("NotifyDataSetChanged")
+                    override fun onResponse(
+                        call: Call<ContentResponseArray>,
+                        response: Response<ContentResponseArray>
+                    ) {
+                        if (response.isSuccessful) {
+                            response.body()?.content?.let {
+                                contentList.addAll(it)
+                                filteredContentList.addAll(it)
+                                contentAdapter.notifyDataSetChanged()
+
+                                Log.i(
+                                    "CONTEÚDO: ENTROU NO ISSUCCESSFUL",
+                                    "Call: ${call} Response: ${response.body()}"
+                                )
+                            }
+                        } else {
+                            Log.i(
+                                "CONTEÚDO: ENTROU NO IF DO ISSUCCESSFUL MAS CAIU NO ELSE",
+                                "Call: ${call}, Response: ${response.code()} ${response.body()})"
+                            )
+                        }
+
+                        loadingView.visibility = View.GONE
                     }
-                }
 
-                override fun onFailure(call: Call<ContentResponseArray>, t: Throwable) {
-                    t.printStackTrace()
-                    Toast.makeText(
-                        this@Content, t.message,
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    override fun onFailure(call: Call<ContentResponseArray>, t: Throwable) {
+                        t.printStackTrace()
+                        Toast.makeText(
+                            this@Content, t.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
 
-                    Log.e(
-                        "ERRO AO PUXAR CONTEUDO",
-                        "Call: ${call} ${t.message} ${t.printStackTrace()}"
-                    )
-                }
-            })
+                        Log.e(
+                            "ERRO AO PUXAR CONTEUDO",
+                            "Call: ${call} ${t.message} ${t.printStackTrace()}"
+                        )
+
+                        loadingView.visibility = View.GONE
+
+                    }
+                })
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        loadingJob?.cancel()
     }
 
     override fun onItemClick(position: Int) {
@@ -112,5 +157,34 @@ class Content : AppCompatActivity(), RecyclerViewInterface {
         accessContent.putExtra("postedBy", contentList[position].usuario.nome)
         startActivity(accessContent)
     }
+
+    private fun normalizedText(text: String): String {
+        return text.replace(Regex("[^\\p{L}\\p{N}\\s\\p{Pd}]"), "")
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun performSearch(query: String) {
+        filteredContentList.clear()
+
+        if (query.isEmpty()) {
+            filteredContentList.addAll(contentList)
+        } else {
+            for (content in contentList) {
+                val normalizedTitle = normalizedText(content.titulo)
+                if (normalizedTitle.contains(query, ignoreCase = true)) {
+                    filteredContentList.add(content)
+                }
+            }
+        }
+
+        contentAdapter.notifyDataSetChanged()
+
+        if (query.isNotEmpty() && filteredContentList.isEmpty()) {
+            noContentText.visibility = View.VISIBLE
+        } else {
+            noContentText.visibility = View.GONE
+        }
+    }
+
 
 }
